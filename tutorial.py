ceda9e309b50fd1890df69e5b4a1b08f5c75c24a
John Walley <john@walley.org.uk>
2012-02-15 16:13:49 +0000

    Removed references to __future__

--- tutorial.py
+++ tutorial.py
@@ -1,5 +1,4 @@
 import os
-from __future__ import with_statement
 from contextlib import closing
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, jsonify
