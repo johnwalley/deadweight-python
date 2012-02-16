a6a3dfec67d140f8385fa1e01c3acd0980e358af
John Walley <john@walley.org.uk>
2012-02-16 10:58:10 +0000

    Better calculation

--- tutorial.py
+++ tutorial.py
@@ -2,7 +2,7 @@ import os
 from contextlib import closing
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, jsonify
-from math import pow
+import math
 
 # Configuration
 DEBUG = True
@@ -13,11 +13,15 @@ app.config.from_object(__name__)
 
 def calculateTime(weight, type, time):
     boatWeight = 0.0
-    if type == "8":
+    if type == "8+":
         boatWeight = 100.0
-    elif type == "4":
-        boatWeight = 60.0	
-    return int(float(time) / pow(weight + boatWeight, 0.222))
+        rowersWeight = 8 * 80.0
+    elif type == "4+":
+        boatWeight = 60.0
+        rowersWeight = 4 * 80.0		
+    theoreticalMass = 55.0 + boatWeight
+    actualMass = weight + boatWeight
+    return int(float(time) * (actualMass - theoreticalMass) / theoreticalMass)
 
 @app.route('/')
 def index():
@@ -25,9 +29,9 @@ def index():
 
 @app.route('/calculate')
 def calculate():
-    weight = request.args.get('weight', 0, type=float)
-    _type = request.args.get('type', "Unknown", type=str)
-    _time = request.args.get('time', 0, type=float)
+    weight = request.args.get('weight', 55, type=float)
+    _type = request.args.get('type', "8+", type=str)
+    _time = request.args.get('time', 360, type=float)
     return jsonify(time=calculateTime(weight, _type, _time))
 
 if __name__ == '__main__':
